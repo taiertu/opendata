2041d44f76dfbb7f21259e5f67fc132e540a5694
claire.tu <taiertu@gmail.com>
2015-10-07 12:26:53 -0400

    final products

--- dataclean.R
+++ dataclean.R
@@ -1,10 +1,11 @@
 library(jsonlite)
 library(dplyr)
+library(googleVis)
 
 ######################################
 #### json to dataframe processing ####
 
-opendata <- fromJSON("./OpenData/arcgis_flatfile (26AUG15).json")
+opendata <- fromJSON("./OpenData/final/arcgis_flatfile.json")
 
 ####### 
 #######inspect the data
@@ -13,18 +14,18 @@ opendata$results$createdAt[[1]]
 opendata$results$org_hq_country[[1]]
 #[1] "Australia"
 opendata$results$org_hq_country_income_code[[300]]
-#[1] "NA"
+#[1] "HIC"
 
 length(opendata$results$org_hq_country_income_code)
-#[1] 3464
+#[1] 4184
 
 table(opendata$results$org_hq_country_income_code)
 #HIC  LIC  LMC  UMC 
-#2013   52  167  614 
+#2094   52  170  656 
 
 sum(table(opendata$results$org_hq_country_income_code))
-#[1] 2846
-## 618 records are NAs.
+#[1] 2972
+## 1212 records are NAs.
 
 # take a look at which countries are missing income code
 for (i in 1:length(opendata$results$org_hq_country_income_code)){
@@ -36,18 +37,20 @@ for (i in 1:length(opendata$results$org_hq_country_income_code)){
 
 table(opendata$results$org_type)
 #Developer group      For-profit       Nonprofit           Other 
-#           279            2235             762             178  
+#           300            2709             953             221  
+
+table(opendata$results$org_type_other)
+sum(table(opendata$results$org_type_other))
 
 table(opendata$results$org_greatest_impact)
-#Economic 1045, Environmental 349, Governance 758, Other 152, Social 1134
+#Economic 1280, Environmental 421, Governance 850, Other 189, Social 1441, NULL (2)
 
 table(opendata$results$org_size_id)
 # 1-10    1000+    11-50 201-1000   51-200 
-#1103      220      689       87      212 
+#1330      347      832       98      274 
 
 ####### 
 ####### convert to dataframe (individual record as row)
-remove(i)
 org_name = c()
 industry_id = c()
 latitude = c()
@@ -58,6 +61,7 @@ org_hq_country = c()
 org_hq_country_code = c()
 org_type = c()
 org_size_id = c()
+org_year_founded = c()
 data_type = c()
 profileid = c()
 objectid = c()
@@ -72,45 +76,61 @@ for (i in 1:length(opendata$results$org_name)){
   org_hq_country_code = c(org_hq_country_code, opendata$results$org_hq_country_locode[[i]])
   org_type = c(org_type, opendata$results$org_type[[i]])
   org_size_id = c(org_size_id, opendata$results$org_size_id[[i]])
+  org_year_founded = c(org_year_founded, opendata$results$org_year_founded[[i]])
   data_type = c(data_type, opendata$results$data_type[[i]])
   profileid = c(profileid, opendata$results$profile_id[[i]])
   objectid = c(objectid, opendata$results$objectId[[i]])
 }
 
 od.df = data.frame(org_name,industry_id,latitude,longitude,
-                   org_type, org_size_id, org_greatest_impact, data_type, 
-                   org_hq_city, org_hq_country, org_hq_country_code, objectid)
+                   org_type, org_size_id, org_year_founded, org_greatest_impact, data_type, 
+                   org_hq_city, org_hq_country, org_hq_country_code, objectid, stringsAsFactors=FALSE)
 
-# Clean the data: country name AU stand for Australia
+# Clean the data: AU --> Australia, México --> Mexico, Hong Kong --> Hong Kong SAR, China
+# 
+table(od.df$org_hq_country)
 od.df$org_hq_country[od.df$org_hq_country == "AU"] <- "Australia"
-summary(od.df$org_hq_country)
+od.df$org_hq_country[od.df$org_hq_country == "México"] <- "Mexico"
+od.df$org_hq_country[od.df$org_hq_country == "Hong Kong"] <- "Hong Kong SAR, China"
+table(od.df$org_hq_country)
+od.df$org_hq_country2 <- od.df$org_hq_country
+
+#### Convert some countries in od.df to align with the names in countrygroup
+od.df$org_hq_country2[od.df$org_hq_country2 == "Egypt"] <- "Egypt, Arab Rep."
+od.df$org_hq_country2[od.df$org_hq_country2 == "Democratic Republic of the Congo"] <- "Congo, Dem. Rep."
+od.df$org_hq_country2[od.df$org_hq_country2 == "Venezuela"] <- "Venezuela, RB"
+
+table(od.df$org_hq_country2)
 
 ##### save to Rds and load country income group data from world bank
-save(od.df, file = "./OpenData/od.df.Rds")
-load("/Users/yichun/OpenData/countrygroup.Rds")
+save(od.df, file = "./OpenData/final/od.df.Rds")
+load("/Users/yichun/OpenData/final/countrygroup.Rds")
 
 which(is.na(countrygroup$Country))
 #integer(0)
 
 ##### Merge open data frame(od.df) with income group data
-od.df_ctrygroup = merge(x = od.df, y = countrygroup, by.x = "org_hq_country", by.y = "Country", all.x = TRUE)
-save(od.df_ctrygroup, file = "./OpenData/od.df_ctrygroup.Rds")
+
+od.df_ctrygroup = merge(x = od.df, y = countrygroup, by.x = "org_hq_country2", by.y = "Country", all.x = TRUE)
+save(od.df_ctrygroup, file = "./OpenData/final/od.df_ctrygroup.Rds")
+length(which(is.na(od.df_ctrygroup$Income_Code)))
+#[1] 32
 
 ## Examine the number of open data use cases across countries and country groups
 table(od.df_ctrygroup$Income_Code) 
 #HIC  LIC  LMC  UMC 
-#2645   19  205  570  
-which(is.na(od.df_ctrygroup$Income_Code))
+#2853  34  231  1034  
 
 ## remove the duplicated records
 od.final = unique(od.df_ctrygroup)
 nrow(od.final)
-#[1] 3464
+#[1] 4184
 
-save(od.final, file = "./OpenData/od.final.Rds")
+save(od.final, file = "./OpenData/final/od.final.Rds")
 
 ################## 
 #####Examine ODB rankings
+ODB_2014_Rankings <- read.csv("~/OpenData/ODB_2014_Rankings.csv")
 summary(ODB_2014_Rankings$ODB.Rank)
 #Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 #1.00   22.00   41.00   42.86   64.00   86.00 
@@ -125,55 +145,54 @@ summary(ODB_2014_Rankings$Readiness)
 
 
 # merge opendata and odb tables
-load("/Users/yichun/OpenData/od.final.Rds")
+load("/Users/yichun/OpenData/final/od.final.Rds")
 nrow(od.final)
-which(is.na(od.final$Code))
-#[1]    1    2    3    4    5    6    7    8    9   10   11   12   13   14  709 2957 2958 2959
+length(which(is.na(od.final$Code)))
+#[1] 32
 od.odb = merge(x = od.final, y = ODB_2014_Rankings, by.x = "Code", by.y = "ISO3", all.x = TRUE)
-save(od.odb, file = "./OpenData/od.odb.Rds")
-load("/Users/yichun/OpenData/od.odb.Rds")
-
+save(od.odb, file = "./OpenData/final/od.odb.Rds")
+load("/Users/yichun/OpenData/final/od.odb.Rds")
 
 # examine the association b/w the number of cases and the ODB ranking across countries
-which(is.na(od.odb$Code))
-#[1] 2300 2301 2302 2303 2304 2305 2306
+length(which(is.na(od.odb$Code)))
+#[1] 32
 which(is.na(od.odb$org_hq_country))
 #integer(0)
 
 #####################
 ###### Create a dataframe based on country level
-cty_cases =arrange(summarise(group_by(od.odb, org_hq_country), count = n()), desc(count))
-cty_cases = merge(x = cty_cases, y = countrygroup, by.x = "org_hq_country", by.y = "Country", all.x  = TRUE)
+cty_cases =arrange(summarise(group_by(od.odb, org_hq_country2), count = n()), desc(count))
+cty_cases = merge(x = cty_cases, y = countrygroup, by.x = "org_hq_country2", by.y = "Country", all.x  = TRUE)
 View(cty_cases)
-save(cty_cases, file = "./OpenData/cty_cases.Rds")
+save(cty_cases, file = "./OpenData/final/cty_cases.Rds")
 
 ###### visualization
-geo = gvisGeoChart(cty_cases, locationvar = "org_hq_country", colorvar = "count", options = list(projection="kavrayskiy-vii", colorAxis="{colors:['#f1cddb','#ba064e','#a70546']}"))
+geo = gvisGeoChart(cty_cases, locationvar = "org_hq_country2", colorvar = "count", options = list(projection="kavrayskiy-vii", colorAxis="{colors:['#f1cddb','#ba064e','#a70546']}"))
 plot(geo)
 
 
 ###### examine open data
 table(od.final$Income_Code)
 #HIC  LIC  LMC  UMC 
-#2645   19  205  570  
+#2853   34  231 1034  
 
 prop.table(table(od.final$Income_Code))
 #HIC         LIC         LMC         UMC 
-#0.769118930 0.005524862 0.059610352 0.165745856 
+#0.687138728 0.008188825 0.055635838 0.249036609 
 
 table(od.final$org_type)
 #Developer group      For-profit       Nonprofit           Other 
-#           279            2235             762             178 
+#           300            2709             953             221 
 
 org.type.income.table = with(od.final, table(org_type, Income_Code))
 org.type.income.table
 #org_type           HIC  LIC  LMC  UMC
-#Developer group  186    6   45   39
-#For-profit      1852    0   74  296
-#Nonprofit        460   13   77  203
-#Other            138    0    8   32
+#Developer group  186    7   45    54
+#For-profit      2017    0   86    589
+#Nonprofit        500   27   84    335
+#Other            150    0   15    56
 
 chisq.test(org.type.income.table, simulate.p.value = TRUE)
 #	Pearson's Chi-squared test with simulated p-value (based on 2000 replicates)
 #data:  org.type.income.table
-#X-squared = 248.14, df = NA, p-value = 0.0004998
+#X-squared = 285.78, df = NA, p-value = 0.0004998
--- visualization.R
+++ visualization.R
@@ -8,8 +8,8 @@ library(plotly)
 library(ggthemes)
 library(googleVis)
 
-load("./OpenData/od.final.Rds")
-load("./OpenData/od.odb.Rds")
+load("./OpenData/final/od.final.Rds")
+load("./OpenData/final/od.odb.Rds")
 #places <- read.csv("~/OpenData/opendataindex_data_2015-01-12 2/places.csv")
 
 Sys.setenv("plotly_username"="taiertu")
@@ -52,17 +52,30 @@ scatter.ggplot <- ggplot(data003, aes(x=Readiness,
 
 ###################
 #### plotly plot showing the relationship between ODB readiness, ODB impact, income groups, and number of cases.
-data003$Income_Code <- factor(data003$Income_Code, levels = c("HIC", "UMC", "LMC", "LIC"))
+data004 = data003
+data004$Income_Code <- factor(data004$Income_Code, levels = c("HIC", "UMC", "LMC", "LIC"))
+levels(data004$Income_Code)[levels(data004$Income_Code)=="HIC"] <- "High-income countries"
+levels(data004$Income_Code)[levels(data004$Income_Code)=="UMC"] <- "Upper-middle-income countries"
+levels(data004$Income_Code)[levels(data004$Income_Code)=="LMC"] <- "Lower-middle-income countries"
+levels(data004$Income_Code)[levels(data004$Income_Code)=="LIC"] <- "Low-income countries"
 
-scatter.plotly.1 <- plot_ly(data003, x = Readiness, y = Impact, text = paste0("Country:", data003$org_hq_country),
-                          mode = "markers", color = Income_Code, size = cases) 
+scatter.plotly.1 <- plot_ly(data004, x = Readiness, y = Impact, text = paste0("Country:", data004$org_hq_country),
+                          mode = "markers", color = Income_Code, size = cases) %>% 
+                    layout(title = "Number of open data cases against Open Data Barometer Indexes by income groups")
 
 scatter.plotly.2 <- plot_ly(data003, x = Readiness, y = Impact, text = paste0("Country:", data003$org_hq_country),
-                            mode = "markers", color = Region.x, size = cases)
+                            mode = "markers", color = Region.x, size = cases) %>%
+                    layout(title = "Number of open data cases against Open Data Barometer Indexes by regions")
 
 
 ################
 ##### online map showing the number of cases
+load("/Users/yichun/OpenData/final/cty_cases.Rds")
+View(cty_cases)
+
+country.map = merge(x = countrygroup, y = cty_cases, by.x = "Country", by.y = "org_hq_country2", all.x = TRUE)
+country.map$count[is.na(country.map$count)] <- 0
+
 l <- list(color = toRGB("grey"), width = 0.2)
 g <- list(
   showframe = FALSE,
@@ -70,99 +83,215 @@ g <- list(
   projection = list(type = 'kavrayskiy-vii')
 )
 
-map.plotly = plot_ly(data003, z = cases, 
-          text = paste0(paste0("\nCountry:", data003$org_hq_country), paste0("\nCases:", data003$cases), paste0("\nIncome Group:", data003$Income_Code)), 
-           locations = Code, type = 'choropleth', color = cases, colors = 'Blues', marker = list(line = l),
-           colorbar = list(title = 'Total Number of Open Data Use Cases'), 
+map.plotly = plot_ly(country.map, z = count, 
+          text = paste0(paste0("\nCountry: ", country.map$Country), paste0("\nCases: ", country.map$count), paste0("\nIncome Group: ", country.map$Income_Code.x)), 
+           locations = Code.x, type = 'choropleth', color = count, 
+          colorscale= list(list(0,"#F7FBFF"), list(0.1,"#5AA1CF"), list(0.4,"#3987C0"), list(0.6,"#1D6AAF"), list(0.8,"#084D96"), list(1,"#08306B")), marker = list(line = l),
+           colorbar = list(title = 'Mapping of open data use cases'), 
            filename="r-docs/world-choropleth") %>%
-  layout(title = 'Total Number of Open Data Use Cases',
-         geo = g)
+  layout(title = 'Mapping of open data use cases', geo = g)
+
+colorscale= list(list(0 = "#F7FBFF"), list(0.5 = "#1D6AAF"), list(1 = "#08306B"))
+colors = 'Blues', 
+
+#{colors:['#f1cddb','#ba064e','#a70546']}"
 
 
 ################
 #### Bar plot showing organization types by income groups
 od.final$Income_Code <- factor(od.final$Income_Code, levels = c("HIC", "UMC", "LMC", "LIC"))
-od.final$org_type <- factor(od.final$org_type, levels = c("Other", "Developer group", "Nonprofit", "For-profit"))
+od.final2 <- od.final
+od.final2 <- od.final2[!(is.na(od.final2$org_type)),]
+od.final2 <- od.final2[!(is.na(od.final2$Income_Code)),]
+levels(od.final2$Income_Code)[levels(od.final2$Income_Code)=="HIC"] <- "High income"
+levels(od.final2$Income_Code)[levels(od.final2$Income_Code)=="UMC"] <- "Upper middle income"
+levels(od.final2$Income_Code)[levels(od.final2$Income_Code)=="LMC"] <- "Lower middle income"
+levels(od.final2$Income_Code)[levels(od.final2$Income_Code)=="LIC"] <- "Low income"
+od.final2$org_type <- factor(od.final2$org_type, levels = c("Other", "Developer group", "Nonprofit", "For-profit"))
+od.final2$Income_Code <- factor(od.final2$Income_Code, levels = c("High income", "Upper middle income", "Lower middle income", "Low income"))
 
-p1 = od.final %>% count(Income_Code, org_type) %>% 
+p1.x = list(title = "Income Groups")
+p1.y = list(title = "Case Counts")
+
+p1 = od.final2 %>% count(Income_Code, org_type) %>% 
   plot_ly(x = Income_Code, y = n, type = "bar", 
           color = org_type, 
           colors = c("#FFDF00","#B1D91B","#187A5C", "#003347"), 
           filename="r-docs/bar-color-map") %>%
-          layout(title = "The Number of Open Data Cases by Organization Types across Income Groups")
+          layout(title = "Number of open data cases by organization types across income groups", xaxis = p1.x, yaxis = p1.y)
+
+data.t = od.final2 %>% group_by(Income_Code, org_type) %>%
+  summarise(total.type = length(Income_Code)) 
+View(data.t)
+
+data.t2 = data.t %>% summarise(total = sum(total.type))
+
+data.t = merge(x = data.t, y = data.t2, by = "Income_Code")
+data.t$perc = data.t$total.type / data.t$total
+
+data.t$Income_Code <- factor(data.t$Income_Code, levels = c("High income", "Upper middle income", "Lower middle income", "Low income"))
+data.t <- data.t[order(data.t$Income_Code),]
+
+perc.p1.x = list(title = "Income Groups")
+perc.p1.y = list(title = "Percentage")
+
+perc.p1 = data.t %>% plot_ly(x = Income_Code, y = perc, type = "bar",
+          color = org_type, 
+          colors = c("#FFDF00","#B1D91B","#187A5C", "#003347"),
+          filename="r-docs/bar-color-map") %>%
+          layout(barmode = "stack", barnorm = "percent", title = "The share of organization types across income groups", xaxis = perc.p1.x, yaxis = perc.p1.y)
 
 ###############
 #### Bar plot showing organization greatest impact by income groups
 #replace the white space in org_greatest_impact to "Other"
-od.final$org_greatest_impact[od.final$org_greatest_impact== ""] <- "Other"
-od.final$org_greatest_impact <- factor(od.final$org_greatest_impact, levels = c("Other", "Environmental", "Governance", "Economic", "Social"))
+od.final2$org_greatest_impact[od.final2$org_greatest_impact== ""] <- "Other"
+od.final2$org_greatest_impact <- factor(od.final2$org_greatest_impact, levels = c("Other", "Environmental", "Governance", "Economic", "Social"))
 
-p2 = od.final %>% count(Income_Code, org_greatest_impact) %>% 
+p2 = od.final2 %>% count(Income_Code, org_greatest_impact) %>% 
   plot_ly(x = Income_Code, y = n, type = "bar", 
           color = org_greatest_impact, 
           colors = c("#880A00", "#FD5E00","#FFDF00","#B1D91B","#187A5C", "#003347"), 
           filename="r-docs/bar-color-map") %>%
-          layout(title = "The Number of Open Data Cases by Organization Impacts across Income Groups")
+          layout(title = "Number of open data cases by organization impacts across income groups")
 
 ###############
 ##### the number of data type 
-table(od.final$data_type)
+table(od.final2$data_type)
 
-p3 = od.final %>% count(data_type) %>%
-  plot_ly(x = data_type, y = n, type = "bar", marker = list(color = toRGB("blue")),
-          filename = "r-docs/bar-custom-colors") %>%
-          layout(title = "The Number of Open Data Cases against Open Data Types")
+datatype.df = od.final2 %>% group_by(data_type) %>% summarise(total = length(data_type))
+datatype.df = datatype.df[!(is.na(datatype.df$data_type) | datatype.df$data_type == ""), ]
+datatype.df = datatype.df[order(-datatype.df$total),]
 
-p4 = od.final %>% count(industry_id) %>%
-  plot_ly(x = industry_id, y = n, type = "bar", marker = list(color = toRGB("blue")),
+p3.x = list(title = "Data Type")
+p3.y = list(title = "Case Counts")
+
+p3 = datatype.df %>% plot_ly(x = data_type, y = total, type = "bar", marker = list(color = toRGB("blue")),
           filename = "r-docs/bar-custom-colors") %>%
-  layout(title = "The Number of Open Data Cases across Industry Id")
+          layout(title = "Number of open data cases against open data types", xaxis = p3.x, yaxis = p3.y)
+
+dataindustry = od.final2 %>% group_by(industry_id) %>% summarise(total = length(industry_id))
+dataindustry = dataindustry[!(is.na(dataindustry$industry_id) | dataindustry$industry_id == ""), ]
+dataindustry = dataindustry[order(-dataindustry$total),]
+
+p4.x = list(title = "Industry ID")
+p4.y = list(title = "Case Counts")
+
+p4 = dataindustry %>% plot_ly(x = industry_id, y = total, type = "bar", marker = list(color = toRGB("orange")),
+          filename = "r-docs/bar-custom-colors") %>%
+  layout(title = "Number of open data cases across industry Id", xaxis = p4.x, yaxis = p4.y)
 
 
 ###############
 ###### Organization size relative to income groups, greatest impact, organization type
-od.final$Income_Code <- factor(od.final$Income_Code, levels = c("HIC", "UMC", "LMC", "LIC"))
-od.final$org_size_id <- factor(od.final$org_size_id, levels = c("1-10", "11-50", "51-200", "201-1000", "1000+"))
+od.final2$org_size_id <- factor(od.final2$org_size_id, levels = c("1-10", "11-50", "51-200", "201-1000", "1000+"))
+od.final3 <- od.final2[!(is.na(od.final2$org_size_id)),] 
 
-p.size = od.final %>% count(org_size_id) %>%
-  plot_ly(x = org_size_id, y = n, type = "bar", marker = list(color = toRGB("orange")),
+p.size.x = list(title = "Organization Size")
+p.size.y = list(title = "Case Counts")
+
+p.size = od.final3 %>% count(org_size_id) %>%
+  plot_ly(x = org_size_id, y = n, type = "bar", marker = list(color = "rgb(44, 160, 44)"),
           filename = "r-docs/bar-custom=colors") %>%
-  layout(title = "The Number of Open Data Cases against Organization Size")
+  layout(title = "Number of open data cases against organization size", xaxis = p.size.x, yaxis = p.size.y)
 
-p5 = od.final %>% count(Income_Code, org_size_id) %>% 
-  plot_ly(x = Income_Code, y = n, type = "bar", 
-          color = org_size_id, 
-          colors = c("#880A00", "#FD5E00","#FFDF00","#B1D91B","#187A5C", "#003347"), 
-          filename="r-docs/bar-color-map") %>%
-  layout(title = "The Size of Organziations against Income Group")
+#p5 = od.final3 %>% count(Income_Code, org_size_id) %>% 
+  #plot_ly(x = Income_Code, y = n, type = "bar", 
+   #       color = org_size_id, 
+    #      colors = c("#880A00", "#FD5E00","#FFDF00","#B1D91B","#187A5C", "#003347"), 
+    #      filename="r-docs/bar-color-map") %>%
+  #layout(title = "The size of organization against income groups")
 
-p6 = od.final %>% count(org_type, org_size_id) %>% 
-  plot_ly(x = org_type, y = n, type = "bar",
+data.orgsize = od.final3 %>% group_by(Income_Code, org_size_id) %>%
+  summarise(total.type = length(Income_Code)) 
+View(data.orgsize)
+
+data.orgsize2 = data.orgsize %>% summarise(total = sum(total.type))
+
+data.orgsize = merge(x = data.orgsize, y = data.orgsize2, by = "Income_Code")
+data.orgsize$perc = with(data.orgsize, total.type / total)
+
+data.orgsize$Income_Code <- factor(data.orgsize$Income_Code, levels = c("High income", "Upper middle income", "Lower middle income", "Low income"))
+data.orgsize <- data.orgsize[order(data.orgsize$Income_Code),]
+
+perc.p5.x = list(title = "Income Groups")
+perc.p5.y = list(title = "Percentage")
+
+perc.p5 = data.orgsize %>% plot_ly(x = Income_Code, y = perc, type = "bar",
+                             color = org_size_id, 
+                             colors = c("#880A00", "#FD5E00","#FFDF00","#B1D91B","#187A5C", "#003347"),
+                             filename="r-docs/bar-color-map") %>%
+  layout(barmode = "stack", barnorm = "percent", title = "The share of different organization sizes across income groups", xaxis = perc.p5.x, yaxis = perc.p5.y)
+
+data.typesize = od.final3 %>% group_by(org_type, org_size_id) %>%
+  summarise(total.type = length(org_type))
+View(data.typesize)
+
+data.typesize2 = data.typesize %>% summarise(total = sum(total.type))  
+
+data.typesize = merge(x = data.typesize, y = data.typesize2, by = "org_type")
+data.typesize$perc = with(data.typesize, total.type / total)
+
+perc.p6.x = list(title = "Organization Type")
+perc.p6.y = list(title = "Percentage")
+
+perc.p6 = data.typesize %>% plot_ly(x = org_type, y = perc, type = "bar",
           color = org_size_id,
           colors = c("#880A00", "#FD5E00","#FFDF00","#B1D91B","#187A5C", "#003347"),
           filename = "r-docs/bar-color-map") %>%
-  layout(title = "The Size of Organziations against Organization Type")
+  layout(barmode = "stack", barnorm = "percent", title = "The share of different organization sizes against organization types", xaxis = perc.p6.x, yaxis = perc.p6.y)
 
-p7 = od.final %>% count(org_size_id, org_greatest_impact) %>% 
-  plot_ly(x = org_size_id, y = n, type = "bar",
+data.sizeimpact = od.final3 %>% group_by(org_size_id, org_greatest_impact) %>%
+  summarise(total.type = length(org_size_id))
+data.sizeimpact = data.sizeimpact[!(is.na(data.sizeimpact$org_greatest_impact)),]
+View(data.sizeimpact)
+
+data.sizeimpact2 = data.sizeimpact %>% summarise(total = sum(total.type))  
+
+data.sizeimpact = merge(x = data.sizeimpact, y = data.sizeimpact2, by = "org_size_id")
+data.sizeimpact$perc = with(data.sizeimpact, total.type / total)
+
+perc.p7.x = list(title = "Organization Size")
+perc.p7.y = list(title = "Percentage")
+
+data.sizeimpact$org_size_id <- factor(data.sizeimpact$org_size_id, levels = c("1-10", "11-50", "51-200", "201-1000", "1000+"))
+data.sizeimpact <- data.sizeimpact[order(data.sizeimpact$org_size_id),]
+
+perc.p7 = data.sizeimpact %>%  plot_ly(x = org_size_id, y = perc, type = "bar",
           color = org_greatest_impact,
           colors = c("#880A00", "#FD5E00","#FFDF00","#B1D91B","#187A5C", "#003347"),
           filename = "r-docs/bar-color-map") %>%
-  layout(title = "The Organization Greatest Impact by Different Organization Sizes")
-  
+  layout(barmode = "stack", barnorm = "percent", title = "The share of organization greatest impacts across different organization sizes", xaxis = perc.p7.x, yaxis = perc.p7.y)
+
+#### year founded
+table(od.final3$org_year_founded)
+od.final3$year <- cut(od.final3$org_year_founded, c(0, 1960, 1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020), right = FALSE)
+
+data.year.1 <- od.final3 %>% group_by(org_name, year, org_greatest_impact) %>% summarise(total.cases = length(org_name))
+data.year.1 <- data.year.1[!(is.na(data.year.1$year) | (data.year.1$org_greatest_impact == "")),]
+data.year.2 <- data.year.1 %>% group_by(year,org_greatest_impact) %>% summarise(total = length(org_greatest_impact))
+data.year.2$org_greatest_impact <- factor(data.year.2$org_greatest_impact, levels = c("Other", "Environmental", "Governance", "Economic", "Social"))
+
+#data.year.3 <- od.final3 %>% group_by(org_name, year, Income_Code) %>% summarise(total.cases = length(org_name))
+#data.year.3 <- data.year.3[!(is.na(data.year.3$year)),]
+#data.year.4 <- data.year.3 %>% group_by(year, Income_Code) %>% summarise(total = length(Income_Code))
+
+#perc.impactyear = data.year.2 %>% plot_ly(x = year, y = total, type = "bar",
+ #                                   color = org_greatest_impact,
+  #                                  colors = c("#880A00", "#FD5E00","#FFDF00","#B1D91B","#187A5C", "#003347"),
+   #                                 filename = "r-docs/bar-color-map") %>%
+  #layout(barmode = "stack", barnorm = "percent", title = "Share of organization impacts by founded year", xaxis = list(title = "Year"), yaxis = list(title = "Percentage"))
+
+impactyear = data.year.2 %>% plot_ly(x = year, y = total, type = "bar",
+                                     color = org_greatest_impact,
+                                     colors = c("#880A00", "#FD5E00","#FFDF00","#B1D91B","#187A5C", "#003347"),
+                                     filename = "r-docs/bar-color-map") %>%
+  layout(barmode = "stack", title = "Number of organizations founded by years and impacts", xaxis = list(title = "Year"), yaxis = list(title = "Number of Organizations Founded"))
 
 ###############
 ##### another online map showing the number of cases
-load("/Users/yichun/OpenData/cty_cases.Rds")
-#View(cty_cases)
-geo = gvisGeoChart(cty_cases, locationvar = "org_hq_country", 
+load("/Users/yichun/OpenData/final/cty_cases.Rds")
+View(cty_cases)
+geo = gvisGeoChart(cty_cases, locationvar = "org_hq_country2", 
                    colorvar = "count",  
                    options = list(projection="kavrayskiy-vii", colorAxis="{colors:['#f1cddb','#ba064e','#a70546']}"))
 plot(geo)
-
-
-
-
-
-
-
